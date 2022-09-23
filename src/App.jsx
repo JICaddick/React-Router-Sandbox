172506aaa41c949ec7b730af3ff037d23e286481
JICaddick <james_caddick16@hotmail.com>
2022-09-23 11:19:20 -0500

    router tutorial finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet, Link } from "react-router-dom";
 
 export default function App() {
+  
   return (
     <div>
       <h1>Bookkeeper!</h1>
--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router-dom";
 //passing in getInvoice from data.js to get the rest of the information behind the invoice number
-import { getInvoice } from "../data"
+import {
+  useParams,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { getInvoice, deleteInvoice } from "../data";
     
 export default function Invoice() {
     let params = useParams();
     // We use parseInt because URL params are always strings. The numbers rendered are part of the string. 
+    let navigate = useNavigate();
+    let location = useLocation();
     let invoice = getInvoice(parseInt(params.invoiceId, 10));
     return(
     <main style={{ padding: "1rem" }}>
@@ -13,6 +19,16 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number);
+            navigate("/invoices" + location.search);
+          }}
+        >
+          Delete
+          </button>
+          </p>
     </main>
   );
 }
